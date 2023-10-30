bea960ae061b7b62c2ebb2174d5e84bff077c382
Malik120456 <149401170+Malik120456@users.noreply.github.com>
2023-10-30 23:02:00 +0700

    Add files via upload

---
+++ warmindo.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2023 at 05:03 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spkwpwarkop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warmindo`
+--
+
+CREATE TABLE `warmindo` (
+  `nama_warmindo` text DEFAULT NULL,
+  `kualitas` int(11) DEFAULT NULL,
+  `pelayanan` int(11) DEFAULT NULL,
+  `jarak` text DEFAULT NULL,
+  `suasana` text DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
